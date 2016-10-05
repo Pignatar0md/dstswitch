bc978209452fa690e9a13c07dc26cd874b0c8b71
Pignatar0md <pignataromd@gmail.com>
2016-10-05 13:19:20 -0300

    3rd fixing DstAGI

--- helpers/DstAGI.php
+++ helpers/DstAGI.php
@@ -48,49 +48,49 @@ $nroDiscado = $Agi->get_variable("agi_dnid", true);
 $toCall = '';
 
 if ($nroDiscado) {
-    if (count($nroDiscado) === 12) {
+    if (strlen($nroDiscado) === 12) {
         //12 digitos = celular larga distancia
         if ($dest == "CelularesInterurbano") {
             $toCall = true;
         } else {
             $toCall = false;
         }
-    } elseif (count($nroDiscado) === 10 && substr($nroDiscado, 0, 1) === 0) {
-        //10 = fijo larga distancia   
-        if ($dest == "FijosInterurbanos") {
-            $toCall = true;
-        } else {
-            $toCall = false;
-        }
-    } elseif (count($nroDiscado) === 7) {
-        //7 digitos de largo = fijo local
-        if ($dest == "FijosUrbanos") {
-            $toCall = true;
-        } else {
-            $toCall = false;
-        }
-    } elseif (count($nroDiscado) === 9 && substr($nroDiscado, 0, 2) === 15) {
-        //9 digitos de largo con 15 = celular local
-        if ($dest == "CelularesUrbanos") {
-            $toCall = true;
-        } else {
-            $toCall = false;
-        }
-    } elseif (count($nroDiscado) === 10 && substr($nroDiscado, 0, 1) === 8) {
+    } elseif (strlen($nroDiscado) === 10 && substr($nroDiscado, 0, 1) === '8') {
          //10 digitos de largo comenzando con 8 = 810/ 800
         if ($dest == "CeroOchocientos") {
             $toCall = true;
         } else {
             $toCall = false;
         }
-    } /*elseif (substr($nroDiscado, 0, 2) === 00) {
+    } elseif (strlen($nroDiscado) === 10) {
+        //10 = fijo larga distancia   
+        if ($dest == "FijosInterurbanos") {
+            $toCall = true;
+        } else {
+            $toCall = false;
+        }
+    } elseif (strlen($nroDiscado) === 7) {
+        //7 digitos de largo = fijo local
+        if ($dest == "FijosUrbanos") {
+            $toCall = true;
+        } else {
+            $toCall = false;
+        }
+    } elseif (strlen($nroDiscado) === 9 && substr($nroDiscado, 0, 2) === '15') {
+        //9 digitos de largo con 15 = celular local
+        if ($dest == "CelularesUrbanos") {
+            $toCall = true;
+        } else {
+            $toCall = false;
+        }
+    } elseif (strlen($nroDiscado) > '12') {
          //empezando con 00 es larga distancia internacional
         if ($dest == "Internacionales") {
             $toCall = true;
         } else {
             $toCall = false;
         }
-    }*/ else {
+    } else {
         $toCall = 404;
     }
 }
